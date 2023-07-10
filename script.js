b2c0da4f7bb33397cdb8270eedc22f0a11a763a5
Gabriela Menezes <gabimenezess10@gmail.com>
2023-07-09 22:17:10 -0300

    feat: add async function to search for a cep

---
+++ script.js
@@ -0,0 +1,16 @@
+async function buscaEndereco(cep) {
+    try {
+        const consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        const consultaCEPConvertida = await consultaCEP.json();
+        if(consultaCEPConvertida.erro) {
+            throw Error('CEP não existente');
+        }
+        console.log(consultaCEPConvertida)
+        return consultaCEPConvertida
+    } catch (erro) {
+        console.log(erro)
+    }
+}
+
+var cep = document.getElementById('cep')
+cep.addEventListener('focusout', () => buscaEndereco(cep.value))
